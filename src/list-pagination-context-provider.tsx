77ea093e61349eafcc724c82cdf2f3ebfda7197f
jmjkmbm116p <jmjkmbm116p@jmjkmbm116ps-Laptop.local>
2023-02-26 23:40:11 -0800

    initial commit to backup branch

--- src/list-pagination-context-provider.tsx
+++ src/list-pagination-context-provider.tsx
@@ -14,75 +14,112 @@ type PaginationMeta = {
   totalPages: number;
 };
 
-type Pagination = PaginationState & PaginationMeta; // Intersection Type Literals
-type PaginationArgs = Pick<PaginationState, 'totalItems' | 'pageSize' | 'currentPage'>;
+// Intersection Type Literals
+type Pagination = PaginationState & PaginationMeta;
+
+const INITIAL_PAGE_NUMBER = 1;
 
 export const usePaginationContext = create<{
-  // state
   pagination: Pagination;
   setPagination: (pg: Pagination) => void;
   setNextPage: () => void;
+  setPrevPage: () => void;
   setFirstPage: () => void;
-  //
 }>((set, get) => ({
   pagination: {
-    currentPage: 0,
+    currentPage: INITIAL_PAGE_NUMBER,
     pageSize: 0,
     totalItems: 0,
     previousEnabled: false,
     nextEnabled: false,
     totalPages: 0,
   },
-  setPagination: (args: PaginationArgs) => set({ pagination: args }),
+  setPagination: (args: Pagination) => set({ pagination: args }),
   setNextPage: () => {
-    if (get().pagination.currentPage < get().pagination.totalPages) {
-      set((state) => ({
-        pagination: { ...state.pagination, currentPage: state.pagination.currentPage + 1 },
-      })),
-  }},
-  // const { pagination } = get();
-  // const { currentPage, totalPages } = pagination;
+    const { pagination } = get();
+    const { currentPage, totalPages } = pagination;
 
-  // if (currentPage < totalPages) {
-  //   set({
-  //     pagination: {
-  //       ...pagination,
-  //       currentPage: currentPage + 1,
-  //       nextEnabled: true,
-  //     },
-  //   });
-  // } else {
-  //   set({
-  //     pagination: {
-  //       ...pagination,
-  //       nextEnabled: false,
-  //     },
-  //   });
-  // }
-  setPrevPage: () => {},
-  setFirstPage: () => {},
+    if (currentPage < totalPages) {
+      const nextPageNumber = currentPage + 1;
+
+      set({
+        pagination: {
+          ...pagination,
+          currentPage: nextPageNumber,
+          nextEnabled: nextPageNumber < totalPages,
+        },
+      });
+    } else {
+      set({
+        pagination: {
+          ...pagination,
+          nextEnabled: false,
+        },
+      });
+    }
+  },
+  setPrevPage: () => {
+    const { pagination } = get();
+    const { currentPage, totalPages } = pagination;
+
+    if (currentPage > 1 || currentPage <= totalPages) {
+      const prevPageNumber = currentPage - 1;
+
+      set({
+        pagination: {
+          ...pagination,
+          currentPage: prevPageNumber,
+          previousEnabled: prevPageNumber > 1,
+        },
+      });
+    } else {
+      set({
+        pagination: {
+          ...pagination,
+          previousEnabled: false,
+        },
+      });
+    }
+  },
+  setFirstPage: () => {
+    const { pagination } = get();
+    const { currentPage, totalPages } = pagination;
+
+    set({
+      pagination: {
+        ...pagination,
+        currentPage: INITIAL_PAGE_NUMBER,
+        nextEnabled: currentPage < totalPages,
+        previousEnabled: false,
+      },
+    });
+  },
 }));
 
 export interface ListContextProps {
   perPage: number;
-  currentPage: number;
   total: number;
+  currentPage?: number;
 }
 
 type ListPaginationContextProps = ListContextProps;
 
 const ListPaginationContextProvider: FCC<{ value: ListPaginationContextProps }> = ({ children, value }) => {
-  const { pagination, setPagination } = usePaginationContext();
+  const { setPagination } = usePaginationContext();
 
   React.useEffect(() => {
+    const totalPages = Math.ceil(value.total / value.perPage);
+    const currentPage = value.currentPage === undefined || value.currentPage === 0 ? INITIAL_PAGE_NUMBER : value.currentPage;
+    const nextEnabled = currentPage < totalPages;
+    const previousEnabled = currentPage > INITIAL_PAGE_NUMBER || currentPage <= totalPages;
+
     setPagination({
-      totalPages: Math.round(value.total / value.perPage),
-      totalItems: value.total,
-      currentPage: value.currentPage,
+      totalPages,
+      currentPage,
+      nextEnabled,
+      previousEnabled,
       pageSize: value.perPage,
-      // nextEnabled: value.currentPage < pagination.totalPages ? true : false,
-      nextEnabled: pagination.nextEnabled,
-      previousEnabled: value.currentPage > 1 ? true : false,
+      totalItems: value.total,
     });
   }, [value]);
 
